d620d5e7d94189a075f4247ae87d06b4729757ff
Buba-Conteh <contehbuba21@gmail.com>
2022-12-09 17:46:53 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+		char ch;
+
+		   for(ch = 'a' ; ch <= 'z' ; ch++) {
+			         putchar(ch);
+				    }
+		      
+		      return(0);
+}
