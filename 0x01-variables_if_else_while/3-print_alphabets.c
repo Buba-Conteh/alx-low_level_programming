1fd3737451da481a73e49939dd92904debf26fa3
Buba-Conteh <contehbuba21@gmail.com>
2022-12-09 18:33:53 -0800

    refactor

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,19 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-		char ch;
+	char ch;
 
-		   for(ch = 'a' ; ch <= 'z' ; ch++) {
-			         putchar(ch);
-				    }
-		    for(ch = 'A' ; ch <= 'Z' ; ch++) {
-			          putchar(ch);
-				     }
-		     putchar('\n'); 
-		      return(0);
+	 for( ch = 'a' ; ch <= 'z' ; ch++)
+	 putchar(ch);
+
+	 
+
+	 for( ch = 'A' ; ch <= 'Z' ; ch++) 
+	 putchar(ch);
+
+	 
+		   
+       	 putchar('\n');
+
+	 return(0);
 }
