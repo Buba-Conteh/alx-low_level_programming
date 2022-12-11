00c400ef28b182aa49aeb21ea8d1d0f3b5203154
Buba-Conteh <contehbuba21@gmail.com>
2022-12-11 22:12:32 +0000

    fix errors

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/*main-Write a program that prints the lowercase */
-/* betty style doc for function main goes there */
+/**
+* main- entry point
+*
+*
+* Return: always 0
+*/
 int main(void)
 {
 	/*main Write a program that prints the */
