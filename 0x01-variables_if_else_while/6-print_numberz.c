5868f6ab08ee50dbeff44c03adde05d5e59fa59a
Buba-Conteh <contehbuba21@gmail.com>
2022-12-11 23:43:50 +0000

    fix buggs cc

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* main -Print all the letters except q and e */
-/* betty style doc for function main goes there */
+/**
+* main- entry point
+*
+*
+* Return: always 0
+*/
 int main(void)
 {
 	int num;
 
-	for( num = 0; num < 10; num++)
-	putchar(num+'0');
+	for (num = 0; num < 10; num++)
+	putchar(num + '0');
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
