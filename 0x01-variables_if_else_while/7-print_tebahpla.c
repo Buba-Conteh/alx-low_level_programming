b53a2562909f7c951c8f294d612122cefb3c3eae
Buba-Conteh <contehbuba21@gmail.com>
2022-12-11 17:28:49 +0000

    reverse alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,10 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-/* Write a program that prints the lowercase alphabet in reverse, followed by a new line.*/
+/*main Write a program that prints the lowercase alphabet in reverse, followed by a new line.*/
 /* betty style doc for function main goes there */
 int main(void)
 {
+	/*main Write a program that prints the lowercase alphabet in reverse, followed by a new line.*/
+
 	char ch;
 
 	for( ch = 'z' ; ch >= 'a' ; ch--)
