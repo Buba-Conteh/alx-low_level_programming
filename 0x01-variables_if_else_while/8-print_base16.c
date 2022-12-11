8d8847fea56c96775bbb9835ff4bc7d716ec1fd5
Buba-Conteh <contehbuba21@gmail.com>
2022-12-11 18:06:41 +0000

    main-Write a program that prints the lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/*main-Write a program that prints the lowercase */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/*main Write a program that prints the */
+
+	char ch;
+
+	for (ch = 'z' ; ch >= 'a' ; ch--)
+	putchar (ch);
+
+	putchar ('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* main-Write a program that prints the lowercase */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	/*main Write a program that prints the */
+
+	char num;
+
+	for( num = 0 ; num < 10 ; num++){
+		if(num == 9)
+		{
+			putchar(num+'0');
+		}else{
+			putchar(num+'0');
+			putchar (',');
+
+		}
+	 
+	}
+
+	return (0);
+}
