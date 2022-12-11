14e9fab4cc8f4df6ae80393551af5d77bfae89f3
Buba-Conteh <contehbuba21@gmail.com>
2022-12-11 21:46:43 +0000

    do all

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,14 @@ int main(void)
 	int num;
 
 	for( num = 0 ; num < 10 ; num++){
-		if(num == 9)
+		if(num != 9)
 		{
 			putchar (num+'0');
-		}else{
-			putchar (num+'0');
 			putchar (',');
+		}
 			putchar (' ');
 
-		}
+		
 	 
 	}
 
