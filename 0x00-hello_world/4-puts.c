3908cee075d750e13be5b26a01f616c7ebc808b0
Buba-Conteh <contehbuba21@gmail.com>
2022-12-12 14:29:59 +0000

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-
+/**
+* main- entry point
+*
+*
+* Return: always 0
+*/
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
