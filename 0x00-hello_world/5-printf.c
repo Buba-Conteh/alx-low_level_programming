14ffbac9a2e1fc6880a7efb95850ade2fcbb3f62
Buba-Conteh <contehbuba21@gmail.com>
2022-12-12 14:58:39 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main- entry point
+*
+*
+* Return: always 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
